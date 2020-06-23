4e16359521acaf9e3962576e95cffaa6aa088a12
hlswyz <937703149@qq.com>
2020-06-23 16:54:39 +0800

    彭贝

---
+++ src/main/java/com/git/HelloGitHub.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("通过GitHub的克隆项目添加内容");
+
+	}
+
+}
